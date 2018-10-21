e7a312c80671273105b1435c3d47575411ade68d
caijungao <943882938@qq.com>
2018-10-21 22:37:10 +0800

    数据库设计

---
+++ ArcFace.Core/Models/Entities/Sys_Log.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArcFace.Core.Models.Entities
+{
+    public class Sys_Log : EntityBase
+    {
+        [Key, Require]
+        public Guid id { get; set; }
+
+        [Require]
+        public Guid user_id { get; set; }
+        
+        public string operation { get; set; }
+
+        public DateTime operate_date { get; set; }
+
+        public string comment { get; set; }
+
+    }
+}
---
+++ ArcFace.Core/Models/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArcFace.Core.Models.Entities
+{
+   public  class User : EntityBase
+    {
+        [Key, Require]
+        public Guid id { get; set; }
+
+        [Require]
+        public string account { get; set; }
+
+        [Require]
+        public string password { get; set; }
+
+        public string user_name { get; set; }
+
+        public DateTime create_date { get; set; }
+
+        public string comment { get; set; }
+       
+    }
+}
